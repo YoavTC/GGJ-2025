ca82df91eb0ae2206c638428ac43322e7da84880
Yoav TC <yoavtc2004@gmail.com>
2025-01-22 23:08:06 +0200

    Initialized repo, added empty project & tools

---
+++ Assets/External Packages/CastVisualizer/Editor/HarmonyLib/Shutup.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+public class Shutup : MonoBehaviour { }
+
+// Script to shut up the warning message
+// "Assembly will not be compiled, because it has no scripts associated with it."
+
+// Don't delete
---
+++ Assets/External Packages/Extra Components/TweenBobEffect.cs
@@ -0,0 +1,23 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class TweenSBobEffect : MonoBehaviour
+{
+    [SerializeField] private Vector2 bobDirection;
+    [SerializeField] private float duration;
+    [SerializeField] private bool loop;
+    [SerializeField] private LoopType loopType;
+    [SerializeField] private bool ignoreTimeScale;
+
+    public void DoEffect()
+    {
+        if (transform != null)
+        {
+            transform.DOKill(true);
+            transform.DOMove(transform.position + (Vector3) bobDirection, duration)
+            
+                .SetLoops(loop ? -1 : 0, loopType)
+                .SetUpdate(ignoreTimeScale);
+        }
+    }
+}
