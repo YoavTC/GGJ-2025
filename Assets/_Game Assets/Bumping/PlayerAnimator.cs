63fb9b5704a206d0fa9140389c10cc985c0cad40
Yoav TC <yoavtc2004@gmail.com>
2025-01-24 16:33:36 +0200

    Finished animator logic

--- Assets/_Game Assets/Bumping/PlayerAnimator.cs
+++ Assets/_Game Assets/Bumping/PlayerAnimator.cs
@@ -1,3 +1,5 @@
+using System;
+using NaughtyAttributes;
 using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
@@ -5,19 +7,38 @@ public class PlayerAnimator : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Animator animator;
 
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] [Layer] private string groundLayerMask;
+    private int _groundLayerMask;
+
     private void Start()
     {
         if (rb == null) rb = GetComponent<Rigidbody>();
         if (animator == null) animator = GetComponent<Animator>();
-    }
 
-    public void Update()
+        _groundLayerMask = LayerMask.GetMask(groundLayerMask);
+    }
+    
+    //  10.021381728937162
+    //  10.02
+    //
+    //
+
+    public void FixedUpdate()
     {
-        animator.SetBool("is_running", Mathf.Abs(rb.linearVelocity.x) > 0f);
+        
+        bool isAirborne = !Physics.CheckSphere(groundCheck.position, groundCheckRadius, _groundLayerMask);
+        bool isRunning = (Math.Floor(Mathf.Abs(rb.linearVelocity.x) * 10 / 10)> 0f) && !isAirborne; 
+
+        Debug.Log(rb.linearVelocity.x);
+        
+        animator.SetBool("is_running", isRunning);
+        animator.SetBool("is_airborne", isAirborne);
     }
 
     public void OnDash()
     {
-        animator.SetTrigger("dash");
+        animator.SetBool("is_leaping", true);
     }
 }
