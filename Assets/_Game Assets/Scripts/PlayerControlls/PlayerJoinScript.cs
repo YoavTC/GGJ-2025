5e0306ba266f48c343248171df34385a38af805a
Yoav TC <yoavtc2004@gmail.com>
2025-01-25 15:23:39 +0200

    Update PlayerJoinScript.cs

--- Assets/_Game Assets/Scripts/PlayerControlls/PlayerJoinScript.cs
+++ Assets/_Game Assets/Scripts/PlayerControlls/PlayerJoinScript.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using NaughtyAttributes;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -14,22 +15,33 @@ public class PlayerJoinScript : MonoBehaviour
     private PlayerInputManager playerInputManager;
     [SerializeField] CinemachineTargetGroup cinemachineTargetGroup;
 
+    private void OnEnable() => playerInputManager.onPlayerJoined += AddPlayerInput;
+    private void OnDisable() => playerInputManager.onPlayerJoined -= AddPlayerInput;
+    
     private void Awake()
     {
         playerInputManager = FindFirstObjectByType<PlayerInputManager>();
+        
+        Gamepad[] gamepads = Gamepad.all.ToArray();
+        InputDevice[] inputDevices = new InputDevice[2];
 
+        // Set first input as Gamepad
+        inputDevices[0] = gamepads[0];
+        
+        // Set second input as either Gamepad or Keyboard
+        if (gamepads.Length == 2)
+        {
+            inputDevices[1] = gamepads[1];
+        } else inputDevices[1] = Keyboard.current;
+        
+        // Join the players with the correct input devices
+        for (int i = 0; i < inputDevices.Length; i++)
+        {
+            playerInputManager.JoinPlayer(i, -1, null, inputDevices[i]);
+        }
     }
-
-    private void OnEnable()
-    {
-        playerInputManager.onPlayerJoined += AddPlayerInput;
-    }
-
-    private void OnDisable()
-    {
-        playerInputManager.onPlayerJoined -= AddPlayerInput;
-    }
-    public void AddPlayerInput(PlayerInput playerInput)
+    
+    private void AddPlayerInput(PlayerInput playerInput)
     {
         playerInputs.Add(playerInput);
         
@@ -41,12 +53,7 @@ public class PlayerJoinScript : MonoBehaviour
         }
         Transform playerParent = playerInput.transform.root;
         playerParent.gameObject.tag = "Player";
-        cinemachineTargetGroup.AddMember(playerParent, 1f, 1f);
+        if (cinemachineTargetGroup != null) cinemachineTargetGroup.AddMember(playerParent, 1f, 1f);
         playerParent.position = spawnPoints[playerInputs.Count - 1].position;
-
-      
-        
-
     }
-
 }
